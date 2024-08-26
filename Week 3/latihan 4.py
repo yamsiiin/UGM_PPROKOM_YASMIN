d487e371992f0adc03f0f41cdd7a6665fe16758a
yamsiiin <aisyahyasminahuwaida@mail.ugm.ac.id>
2024-08-26 15:21:13 +0700

    Add files via upload

---
+++ Week 3/latihan 4.py
@@ -0,0 +1,12 @@
+>>> str="Py'thon"
+>>> str
+>>> str2='Py"thon'
+>>> str2
+>>>"\"OK,\"sampai ketemu lagi"
+>>>teks="Python adalah bahas pemrogaman yang powerfull.\nTerbukti Python bisa dijalankan di segala platform OS.\nJadi, saatnya kita menggunakan Python sebagai bahasa pemrogaman\nsehari-hari. Salam Python Dahsyat!"
+>>>print(teks)
+>>>kata="""Python adalah bahasa pemrogaman yang powerfull.
+...Terbukti Python bisa dijalankan di segala platform OS.
+...Jadi, saatnya kita menggunakan Python sebagai bahasa pemrogaman
+...sehari-hari. Salam Python Dahsyat!"""
+>>>print(kata)
---
+++ Week 3/latihan 5 salah.py
@@ -0,0 +1,13 @@
+blog='Klinik'+'Python'
+blog
+newblog=blog*5
+newblog
+blog*=4
+print(blog)
+string='Klinik''Python'
+string
+buah='Nanas'
+buah[0]
+buah[0:2]
+buah[0:4]
+buah[0:5]
