6f50bfab62b27c526004209ccd4302b9dd1d4564
yamsiiin <aisyahyasminahuwaida@mail.ugm.ac.id>
2024-09-08 21:53:10 +0700

    latihan 4.1.py

---
+++ Week 2/latihan 4.1.py
@@ -0,0 +1,9 @@
+#program pertama
+print("PRAKTIKUM PEMROGAMAN KOMPUTER")
+print("Berikut contoh program penambahan")
+a=2
+b=3
+hasil=a+b
+print("hasil= a + b")
+print("hasil= %d + %d"%(a,b))
+print("hasil= %d"%(hasil))
