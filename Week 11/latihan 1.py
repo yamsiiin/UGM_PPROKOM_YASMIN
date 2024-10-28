07eda2bfd417d2447b7a122738c8fac12b9e56f7
yamsiiin <aisyahyasminahuwaida@mail.ugm.ac.id>
2024-10-28 13:28:29 +0700

    Create latihan 1.py

---
+++ Week 11/latihan 1.py
@@ -0,0 +1,6 @@
+# definisi fungsi print_string
+def print_string( str ):
+    """Menampilkan argumen string str ke layar"""
+    print (str)
+# Kita memanggil fungsi dengan kata kunci
+print_string( str = "Hello Python")
