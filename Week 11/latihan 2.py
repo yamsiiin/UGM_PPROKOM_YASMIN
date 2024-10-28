d74836bb447ae2fafbf58b57164eca03d022e48b
yamsiiin <aisyahyasminahuwaida@mail.ugm.ac.id>
2024-10-28 13:28:54 +0700

    Add files via upload

---
+++ Week 11/latihan 2.py
@@ -0,0 +1,7 @@
+# Definisi fungsi
+def print_info( nama, usia ):
+    """Fungsi ini menampilkan info yang dimasukkan"""
+    print ("Nama: ", nama)
+    print ("Usia: ", usia)
+# Memanggil fungsi
+print_info( usia = 25, nama = "Budi" )
